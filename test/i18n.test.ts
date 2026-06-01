01916f52e40de3202f2eaa084cff4c5033f7cb4c
Bonkers_96 <42640149+l3onkers@users.noreply.github.com>
2026-06-01 14:50:14 +0200

    test: update i18n test to match new html tags in hero title

--- test/i18n.test.ts
+++ test/i18n.test.ts
@@ -13,6 +13,6 @@ describe('i18n translations', () => {
   });
 
   it('should fallback to ES if key not found (if implemented) or just match default dictionary', () => {
-    expect(ui[defaultLang]['hero.title']).toBe('Hola, soy Álvaro');
+    expect(ui[defaultLang]['hero.title']).toBe('Hola, soy <span class="gradient-name">Álvaro</span>');
   });
 });
